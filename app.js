a530f71b0070daa681c990cde05ee34ba08d66a6
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-08-26 13:55:54 -0400

    Part II completed

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ const myMap = {
 		// this = myMap object
 		this.map = L.map('map', {  // Per Usage example at https://leafletjs.com/reference-1.7.1.html#map-factory
 			center: this.coordinates,
-			zoom: 11,
+			zoom: 2,
 		});
 
 		// Add OpenStreetMap tiles:
 		// Per Usage example at https://leafletjs.com/reference-1.7.1.html#tilelayer
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-			minZoom: '15',
+			minZoom: '5',
 		}).addTo(this.map)
 
 		// Add geolocation marker:
@@ -30,14 +30,24 @@ const myMap = {
 		// Per Usage example at https://leafletjs.com/reference-1.7.1.html#marker
 		.bindPopup('<p1><b>You are here</b><br></p1>').openPopup()
 	},
-}
-	// Add business markers
 
+	// Add business markers:
+	addMarkers() {
+		for(var i = 0; i < this.businesses.length; i++) {
+		this.markers = L.marker([
+			this.businesses[i].lat,
+			this.businesses[i].long,
+		])
+			.bindPopup(`<p1>${this.businesses[i].name}</p1>`)
+			.addTo(this.map)
+		}
+	}
+}
 
 
 // Get coordinates via geolocation API:
 // Async function code found at https://stackoverflow.com/questions/51843227/how-to-use-async-wait-with-html5-geolocation-api
-const getCoords = async () => {
+async function getCoords() {
 	const pos = await new Promise((resolve, reject) => {
 	  navigator.geolocation.getCurrentPosition(resolve, reject);
 	});
@@ -46,11 +56,52 @@ const getCoords = async () => {
 
 
 // Get Foursquare businesses:
+// API Key: fsq3RMAC4R0VQaW0WlLCXNVPQctaq2GzzdWBi/29vED9GW0=
+/* Commented out, after checking hardcoded coordinates, business type and limit used to verify correct results.
+const options = {
+		method: 'GET',
+		headers: {
+	  		Accept: 'application/json',
+	  		Authorization: 'fsq3RMAC4R0VQaW0WlLCXNVPQctaq2GzzdWBi/29vED9GW0='
+		}
+	};
+fetch('https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?query=restaurant&ll=35.898565%2C-78.800605&limit=5', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+  	.catch(err => console.error(err));
+*/
 
+// Used above code as in function to access business results
+async function getFoursquare(business) {  // incorporated code from Foursquare into getFoursquare function to return business results
+	const options = {  // code pulled from Foursquare documentation at https://developer.foursquare.com/reference/place-search
+		method: 'GET',
+		headers: {
+	  		Accept: 'application/json',
+	  		Authorization: 'fsq3RMAC4R0VQaW0WlLCXNVPQctaq2GzzdWBi/29vED9GW0='
+		}
+	}
+	let limit = 5  // hardcode number of results to return for selected business
+	let lat = myMap.coordinates[0]  // pull coordinates from myMap object to use as lat and lon in fetch request
+	let lon = myMap.coordinates[1]
+	let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?query=${business}&ll=${lat}%2C${lon}&limit=${limit}`, options)
+	let data = await response.text()
+	let parsedData = JSON.parse(data)
+	let businesses = parsedData.results
+	return businesses
+}
 
-
-// Process foursquare array:
-
+// Process Foursquare array:
+function processBusinesses(data) {
+	let businesses = data.map((element) => {
+		let location = {
+			name: element.name,
+			lat: element.geocodes.main.latitude,
+			long: element.geocodes.main.longitude
+		};
+		return location  // returns object with three properties
+	})
+	return businesses  // returns array of above objects
+}
 
 
 
@@ -59,7 +110,6 @@ const getCoords = async () => {
 window.onload = async () => {
 	const coords = await getCoords()
 	myMap.coordinates = coords
-	console.log(coords) // check if my coordinates, [35.8987086, -78.8008756] are returned
 	myMap.buildMap()
 }
 
@@ -70,5 +120,7 @@ document.getElementById('submit').addEventListener('click', async (event) => {
 	event.preventDefault()
 	// Set dusiness option selected (restaurant, coffeee, etc.) and submitted (clicked on) in dropdown as variable
 	let business = document.getElementById('business').value
-	console.log(business) // check if submit button click logs selected business
+	let data = await getFoursquare(business)
+	myMap.businesses = processBusinesses(data)
+	myMap.addMarkers()
 })
